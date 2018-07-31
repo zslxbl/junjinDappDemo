8866b5dd7e708ad130c4b1940e69f8e358497f25
zslxbl <zslxbl@163.com>
2018-07-31 20:58:12 +0800

    compile onestep

---
+++ scripts/compile.js
@@ -0,0 +1,11 @@
+const fs = require('fs-extra');
+const path = require('path');
+const solc = require('solc');
+
+const contractPath = path.resolve(__dirname, '../contracts', 'Car.sol');
+const contractSource = fs.readFileSync(contractPath, 'utf-8');
+
+const result = solc.compile(contractSource, 1);
+console.log(result);
+
+
